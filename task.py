72dd1fefda8de77a81208ced385106a9036d787f
Vladimir L <vladimir.losk90@gmail.com>
2017-04-11 22:19:52 -0400

    Init

---
+++ task.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+
+def main():
+    pass
+
+    return 0
+
+if __name__ == "__main__":
+    # start_time = time.time()
+    main()
+    # print("\n%s seconds" % (time.time() - start_time))
